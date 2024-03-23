33e942a90eb617f5a1d2651413783e5c50dd2008
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2024-03-23 18:25:13 -0500

    Health Check Microservicio Usuarios

--- backend/usuarios/src/blueprints/usuarios.py
+++ backend/usuarios/src/blueprints/usuarios.py
@@ -4,5 +4,5 @@ usuarios_blueprint = Blueprint('usuarios', __name__, url_prefix="/usuarios")
 
 @usuarios_blueprint.route('/health-check', methods = ['GET'])
 def health_check():
-    return jsonify({'msg':"UP"}),200  
+    return jsonify({"mensaje":"UP"}),200  
  
