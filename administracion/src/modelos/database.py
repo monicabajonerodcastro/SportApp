5b46ab96737b6006684dd810d6faa798a7d550e3
monicabajonerodcastro <m.bajonero@uniandes.edu.co>
2024-03-27 22:37:38 -0500

    Creación servicio administración

--- administracion/src/modelos/database.py
+++ administracion/src/modelos/database.py
@@ -1,13 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from src.utilidades import utilidades_gcp
 import os
 
-postgres_password = utilidades_gcp.obtener_secreto(os.environ["PROJECT_ID"], os.environ["SECRET_PASSWORD_DB_ID"])
-
 engine = create_engine(
-    "postgresql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"], postgres_password, os.environ["DB_HOST"], os.environ["DB_PORT"], os.environ["DB_NAME"]),
+    "postgresql://{}:{}@{}:{}/{}".format(os.environ["DB_USER"], os.environ["DB_PASSWORD"], os.environ["DB_HOST"], os.environ["DB_PORT"], os.environ["DB_NAME"]),
 )
 
 db_session = scoped_session(
--- administracion/test/comandos/test_obtener_plan_por_id.py
+++ administracion/test/comandos/test_obtener_plan_por_id.py
@@ -14,16 +14,29 @@ def mock_session():
 
 @patch('test.mock_session', autospec=True)
 def test_obtener_plan_id(mock_session):
-    plan_mock = plan_mock()
+    mock_plan = plan_mock()
 
     mock_session_instance = mock_session.return_value
     mock_query = mock_session_instance.query.return_value
-    mock_query.filter.return_value.first.return_value = plan_mock
+    mock_query.filter.return_value.first.return_value = mock_plan
     
-    service = ObtenerPlanId(mock_session_instance, plan_mock.email)
-    result = service.execute()
+    service = ObtenerPlanId(session=mock_session_instance, headers={"Authorization": "Bearer"}, id_plan=mock_plan.id)
+    (result, _) = service.execute()
     assert mock_session_instance.query.called
-    assert result.name == plan_mock.name
+    assert result["nombre"] == mock_plan.nombre
+
+@patch('test.mock_session', autospec=True)
+def test_obtener_plan_id_sin_autorizacion(mock_session):
+    mock_plan = plan_mock()
+
+    mock_session_instance = mock_session.return_value
+    mock_query = mock_session_instance.query.return_value
+    mock_query.filter.return_value.first.return_value = mock_plan
+    
+    service = ObtenerPlanId(session=mock_session_instance, headers={}, id_plan=mock_plan.id)
+    (_, status) = service.execute()
+    assert not mock_session_instance.query.called
+    assert status == 403
 
 def plan_mock():
     return Plan(id=fake.uuid4(), nombre=fake.name(), funciones=fake.text(), valor_mensual=fake.pyint())
