cf6d3b38539d0c9d328a724f5155dfa2f3394514
Nicolas_Gomez_Barbosa <n.gomezb2@uniandes.edu.co>
2024-04-08 00:33:06 -0500

    SPR:50 - Se realizan ajustes de conflictos

--- administracion/src/errores/errores.py
+++ administracion/src/errores/errores.py
@@ -18,8 +18,11 @@ class BadRequestError(ApiError):
         self.description = description
 
 class MissingRequiredField(ApiError):
-    code = 400
-    description = "Parámetros requeridos"
+    def __init__(self, code = 404, description="No se encontró un parámetro requerido", parameter: str = None) -> None:
+        self.code = code
+        self.description = description
+        if parameter:
+            self.description = f"No se encontró el parámetro [{parameter}]"
 
 class InvalidFormatField(ApiError):
     code = 400
